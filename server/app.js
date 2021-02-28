93525b44fb1d7f4453e6ac0ec2b23dc46dacac3d
Manuel Pretelin <m_pretelin@hotmail.com>
2021-02-28 11:30:33 -0600

    first commit

---
+++ server/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const firebase = require('firebase');
+const { firebaseConfig } = require("./firebase-config")
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const app = express();
+
+firebase.initializeApp(firebaseConfig);
+let database = firebase.database();
+
+app.listen(port, function () {
+  console.log(`App listening on port ${port}`);
+});
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.post('/door-status', function (req, res) {
+  const doorStatus = req.body.status;
+
+  database.ref("door").set({ status: doorStatus }, (err) => {
+    if (err) {
+      console.log("Failed with err: " + error);
+    } else {
+      console.log("Success");
+    }
+  });
+
+  res.send('[Door] Update Successful');
+});
---
+++ server/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAB7u9h4ed-SnJ8WUSVKweGhexdkodvisE",
+  authDomain: "raspberry-door-8c492.firebaseapp.com",
+  databaseURL: "https://raspberry-door-8c492-default-rtdb.firebaseio.com",
+  projectId: "raspberry-door-8c492",
+  storageBucket: "raspberry-door-8c492.appspot.com",
+  messagingSenderId: "344088213985",
+  appId: "1:344088213985:web:2753097cb3b45ccde8faa0"
+}
